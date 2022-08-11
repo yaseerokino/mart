dd8854bcfe0ab141557c98a94845f4346254a342
Yaseer A. Okino <37089367+yaseerokino@users.noreply.github.com>
2022-08-11 22:41:13 +0100

    feat(server0: create error handler middleware

---
+++ server/src/middlewares/error.middleware.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
+import log from '../utils/logger';
+import ErrorResponse from '../helpers/errorResponse';
+import { STATUS_INTERNAL_SERVER_ERROR, STATUS_BAD_REQUEST, STATUS_CONFLICT } from '../constants';
+
+const errorMiddleware = (error: any, req: Request, res: Response, next: NextFunction) => {
+  try {
+    const status: number = error.status || STATUS_INTERNAL_SERVER_ERROR;
+    const message: string = error.message || 'Something went wrong';
+    const customError = error;
+
+    if (error instanceof ErrorResponse) {
+      return res.status((customError as ErrorResponse).status).json({ success: false, error: customError.message });
+    }
+
+    if (error instanceof ZodError) {
+      const errors = Object.values(error.errors).map((el) => el.message);
+      return res.status(STATUS_BAD_REQUEST).json({ success: false, error: errors });
+    }
+
+    if (error.name === 'ValidationError') {
+      const errors = Object.values(error.errors).map((val: any) => val.message);
+      return res.status(STATUS_BAD_REQUEST).json({ success: false, error: errors });
+    }
+
+    if (error.code === 11000) {
+      return res
+        .status(STATUS_CONFLICT)
+        .json({ success: false, error: 'Duplicate resource', duplicate: error.keyValue });
+    }
+    log.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
+    return res.status(status).json({ success: false, error: message });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export default errorMiddleware;
